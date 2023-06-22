1e9f59c63cc25a8644d0874713358c6fd5c5608b
HiiWee <wpdlzhf159@gmail.com>
2023-06-22 15:54:13 +0900

    init: muscle park project

---
+++ src/test/java/com/park/muscle/MuscleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.muscle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
