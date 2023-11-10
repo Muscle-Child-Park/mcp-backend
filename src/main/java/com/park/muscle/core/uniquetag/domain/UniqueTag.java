a2fd66875620ae9b3e3587961d8cc6dcaf4837c0
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-11 01:51:59 +0900

    feat(unique-tag) : 유니크 태그 업데이트

--- src/main/java/com/park/muscle/core/uniquetag/domain/UniqueTag.java
+++ src/main/java/com/park/muscle/core/uniquetag/domain/UniqueTag.java
@@ -1,15 +1,22 @@
 package com.park.muscle.core.uniquetag.domain;
 
+import com.park.muscle.core.member.domain.Member;
+import com.park.muscle.core.trainer.domain.Trainer;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class UniqueTag {
     private final static int LIMIT_LENGTH = 99999;
 
@@ -21,11 +28,22 @@ public class UniqueTag {
     @Column(name = "unique_tag")
     private String uniqueTag;
 
-    public UniqueTag() {
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+
+    @Builder
+    public UniqueTag(Member member, Trainer trainer) {
         this.uniqueTag = generateUniqueTag();
+        this.member = member;
+        this.trainer = trainer;
     }
 
-    public String getFormattedId() {
+    public String formattedId() {
         String formattedNumber = String.format("#%d", id);
         if (isFormatted()) {
             formattedNumber = String.format("#%05d", id);
