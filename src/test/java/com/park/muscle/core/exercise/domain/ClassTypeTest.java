53edda7e329a62ca4005c9ed8ec9c7b5b81a1124
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-12-04 16:50:17 +0900

    test(exercise) : 수업 타입 테스트

---
+++ src/test/java/com/park/muscle/core/exercise/domain/ClassTypeTest.java
@@ -0,0 +1,30 @@
+package com.park.muscle.core.exercise.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.park.muscle.core.exercise.exception.ExerciseTypeNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ClassTypeTest {
+
+    @DisplayName("수업 타입을 찾을 수 있다.")
+    @Test
+    void 수업_타입() {
+        String type = "pt";
+        ClassType classType = ClassType.findType(type);
+        assertThat(classType).isEqualTo(ClassType.PT);
+    }
+
+    @DisplayName("수업 타입이 올바르지 않다면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"p","t","co-pt"})
+    void 수업_타입_불일치(String invalidType) {
+        assertThatThrownBy(() -> ClassType.findType(invalidType))
+                .isInstanceOf(ExerciseTypeNotFoundException.class)
+                .hasMessageContaining("올바른 운동 타입을 찾을 수 없습니다.");
+    }
+}
